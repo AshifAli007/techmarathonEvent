419d46d878e8759ad665d6e7294a51dc546a4af5
AshifAli007 <theahifkhan@gmail.com>
2022-01-07 22:38:06 +0530

    posters fixed

--- dist/demo2.44794d1a.js
+++ dist/demo2.44794d1a.js
@@ -9683,7 +9683,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53564" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59565" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
